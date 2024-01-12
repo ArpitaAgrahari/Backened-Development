06142fa0e1101c54553bc087c1a5ebac1979110b
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-12 14:58:13 +0530

    Updated Commit

--- example.js
+++ example.js
@@ -1,2 +1,6 @@
 //console.log("Hello World!");
-console.error(new Error("hell! This is not right"));
+// console.error(new Error("hell! This is not right"));
+
+
+let name = 'arpita';
+console.log("Don't mess with me name)!.Don't mess with mee ");
