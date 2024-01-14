736992c4b907f4fd2204284e0061ef4e23af9c89
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-14 23:08:04 +0530

    Updated Commit

--- Practice_Tutorial/people.js
+++ Practice_Tutorial/people.js
@@ -1,5 +1,10 @@
 const people =['Arpita','Aru','Harsh','Akshu'];
+const ages =['20','13','24'];
 console.log(people);
 
 
-module.exports = 'hello';
+// module.exports = 'hello';
+
+module.exports ={
+    people, ages 
+}
