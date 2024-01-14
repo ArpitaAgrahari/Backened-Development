7786be5951afe1b6a60fe1e8238d164f6a29ffbc
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-14 23:12:56 +0530

    Updated Commit

--- Practice_Tutorial/Modules.js
+++ Practice_Tutorial/Modules.js
@@ -1,7 +1,9 @@
 //how to access the external js file or to import to another file.
 // it will help in linking pages.
 
-const a = require('./people');
+// const a = require('./people');
+const {people,ages} = require('./people');
+console.log(people,ages);
 
-console.log(a.people, a.ages);
+// console.log(a.people,a.ages);
 
