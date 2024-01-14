e744bc06a8086fd8bcf2caf25eb401271d4dde9f
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-14 23:31:10 +0530

    Updated Commit

--- Practice_Tutorial/File_Operations.js
+++ Practice_Tutorial/File_Operations.js
@@ -1,15 +1,18 @@
 const fs =require('fs');
 
 // Reading Files
-fs.readFile('E:\\Backened Development\\Practice_Tutorial\\docs\\blog.txt',(err, data)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log(data.toString());
-});
+// fs.readFile('E:\\Backened Development\\Practice_Tutorial\\docs\\blog.txt',(err, data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+// console.log("Last Line")
+
 
 
 // Writing files
+
 
 // directories
 
