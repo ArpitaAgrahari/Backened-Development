21ad1ae51d89b2af0960ddf9ab4f793c0c5bfa1b
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-15 00:00:19 +0530

    Updated Commit

--- Practice_Tutorial/Stream_Data.js
+++ Practice_Tutorial/Stream_Data.js
@@ -3,9 +3,11 @@ const readStream = fs.createReadStream('E:\\Backened Development\\Practice_Tutor
 const writeStream = fs.createWriteStream('E:\\Backened Development\\Practice_Tutorial\\docs\\blog4.txt');
 
 
-readStream.on('data',(chunk)=>{
-    console.log("-------NEW CHUNK-------")
-    console.log(chunk);
-    writeStream.write('\n NEW CHUNK\n');
-    writeStream.write(chunk);
-});
+// readStream.on('data',(chunk)=>{
+//     console.log("-------NEW CHUNK-------")
+//     console.log(chunk);
+//     writeStream.write('\n NEW CHUNK\n');
+//     writeStream.write(chunk);
+// });
+
+readStream.pipe(writeStream);
