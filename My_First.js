b6c2dbcefc513f2d1d18ee3aed28552536e051b9
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-18 11:32:38 +0530

    Updated Commit

--- My_First.js
+++ My_First.js
@@ -1,4 +1,4 @@
-var http=require("http");
+var http=require("http"); //require is amodule that requires to work with web server.
 
 http.createServer(function(req, res){
     res.writeHead(200,{'Content.Type':'text/plain'});     //200 is status check for http for satisfaction taht it is okay to enter tot web
