547622d52dc1109f1d30754cfc300439836339d7
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-18 11:31:07 +0530

    Updated Commit

--- My_First.js
+++ My_First.js
@@ -1,7 +1,7 @@
 var http=require("http");
 
 http.createServer(function(req, res){
-    res.writeHead(200,{'Content.Type':'text/plain'});
+    res.writeHead(200,{'Content.Type':'text/plain'});     //200 is status check for http for satisfaction taht it is okay to enter tot web
     res.end('Hello World');
 
 }).listen(8080);
