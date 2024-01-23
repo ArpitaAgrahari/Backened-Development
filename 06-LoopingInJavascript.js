9c7f4f202d9aa3965f706d0981da8e783c1a27fb
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-23 10:44:31 +0530

    Updated Commit

---
+++ 06-LoopingInJavascript.js
@@ -0,0 +1,25 @@
+//LOOPing in Javascript
+
+
+
+//BREAK condiiton in LOOping
+
+// let i = 0;
+// for(i = 0;i<=10;i++){
+//     console.log("The iteration is:" + i);
+// if(i==3){
+//     break;
+// }
+// }
+
+
+
+//Cotinue comdition in LOOping
+
+// let i = 0;
+// for(i = 0;i<=5;i++){
+//     console.log("the iteration is:" + i);
+//     if(i==4){
+//         continue;
+//     }
+// }
