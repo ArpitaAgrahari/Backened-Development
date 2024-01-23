f843a2baa7b33c6c5a5f655991b789c213449613
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-23 10:25:35 +0530

    Updated Commit

--- 05-Basics_Of_JavaScript.js
+++ 05-Basics_Of_JavaScript.js
@@ -2,15 +2,15 @@
 //Basics of looping in javaascript
 // If condition in javascript
 
-let a = 20;
-if(a>18){
-    console.log("You are an adult now!!");
-}else{
-    console.log("You are still underage.");
-}
+// let a = 20;
+// if(a>18){
+//     console.log("You are an adult now!!");
+// }else{
+//     console.log("You are still underage.");
+// }
 
 
 
-if(new Date().getHours()<18){
-    console.log("Good Day!!");
-}
+// if(new Date().getHours()<18){
+//     console.log("Good Day!!");
+// }
