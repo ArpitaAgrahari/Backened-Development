7bd1839c36eddf10a44943fefe038bcb58e9890a
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-02-01 11:29:54 +0530

    Updated Commit

---
+++ 22-buffer_write.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+var buf = new Buffer(20000);
+console.log("going to open file");
+fs.open('input.txt','r+',function(err,fd){
+    if(err){
+        return console.error(err);
+    }
+    console.log("File opened Successfully.")
+    console.log("Going to read the file");
+    fs.read(fd,buf,0,buf.length,0,function(err,bytes){
+        if(err){
+            console.log(err);
+        }
+        console.log(bytes +"bytes read");
+        if(bytes>0){
+            console.log()
+        }
+    })
+});
