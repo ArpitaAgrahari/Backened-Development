c0d16cbe92de021bea183074960d5ed323c949a4
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-02-15 11:49:48 +0530

    Update file reading and writing logic

--- activity2.js
+++ activity2.js
@@ -1,13 +1,28 @@
-var http=require("http"); //require is amodule that requires to work with web server.
+var http=require("http"); 
+//require is amodule that requires to work with web server.
 
 http.createServer(function(req, res){
+
 const fs = require('fs');
 
+
+//for reading the file
 fs.readFile('input.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('There was an error reading the file!', err);
         return;
     }
-    console.log(data);
+
+    // Modify the data here
+    let modifiedData = data.replace('old text', 'new text');
+
+    fs.writeFile('input.txt', modifiedData, 'utf8', (err) => {
+        if (err) {
+            console.error('There was an error writing to the file!', err);
+        } else {
+            console.log('File has been updated');
+        }
+        //finalizing the content
+    });
 });
 }).listen(8080);
