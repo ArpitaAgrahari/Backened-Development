9203e049343b17f460f3c29ef6885c226df5b688
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-05-25 13:51:32 +0530

    Remove obsolete files and add new JavaScript and HTML examples for modules, loops, arrays, objects, and error handling.

--- 03-REPL_TERMINAL.js
+++
@@ -1,8 +0,0 @@
-//REPL = read eval print loop:
-// it represent the console to do mathematical tasks in node terminal or in command line by using node
-// in command line terminal 
-
-
-//5+6
-//output: will be 11
-//most probably it will run normal console statement and mathematical solutions.
