c61f38cfa3205878b72a594d8439c34b111d21bb
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-05-31 11:11:14 +0530

    Add user array examples and iterate over users with for loop

--- notes/javascript.js
+++ notes/javascript.js
@@ -200,3 +200,24 @@ let totalUsers = users.length;
 for (let i = 0; i < totalUsers; i++) {
     console.log(users[i]); // print Bharat, Harkirat, Deepak, Rahul, Rohit
 }
+
+
+
+let users = ["Harkirat", "Bharat", "Deepak", "Rahul", "Rohit"];
+
+console.log(users); // ["Harkirat", "Bharat", "Deepak", "Rahul", "Rohit"]
+
+// console.log(users[0]); // Harkirat
+// console.log(users[1]); // Bharat
+// console.log(users[2]); // Deepak
+// console.log(users[3]); // Rahul
+// console.log(users[4]); // Rohit
+// console.log(users[5]); // undefined
+
+let totalUsers = users.length; 
+console.log(totalUsers); // 5
+
+
+for (let i = 0; i < totalUsers; i++) {
+  console.log(users[i]); // Harkirat, Bharat, Deepak, Rahul, Rohit
+}
