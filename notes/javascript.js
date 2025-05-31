f6d24be8cf879d3ff0829bee721386cb2bc92a5b
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-05-31 11:05:39 +0530

    Add initial JavaScript examples covering variables, data types, and arrays

---
+++ notes/javascript.js
@@ -0,0 +1,73 @@
+let firstName = "Bharat";
+console.log(firstName); // Bharat
+
+const age = 21;
+console.log(age); // 21
+
+var isStudent = true;
+console.log(isStudent); // true
+
+
+console.log("firstName"); // firstName
+console.log("Bharat"); // Bharat
+
+
+isStudent = "Deepak";
+console.log(isStudent); // Deepak
+
+isStudent = 10;
+console.log(isStudent); // 10
+
+
+// let, const
+
+
+// let
+let isStudent = true;
+
+console.log(isStudent); // true
+isStudent = false;
+console.log(isStudent); // false
+
+
+// const
+const firstName = "Bharat";
+
+console.log("Hello from before"); // Hello from before
+// firstName = 1;   // TypeError: Assignment to constant variable.
+console.log("Hello from after");
+
+
+
+let number = 44; // Number
+let string = "Hello World!"; // String
+let isActive = false; // Boolean
+let numbers = [1, 2, 3]; // Array
+
+console.log(number); // 44
+console.log(string); // Hello World!
+console.log(isActive); // false
+console.log(numbers); // [1, 2, 3]
+
+
+let user1 = "Harkirat";
+let user2 = "Bharat";
+let user3 = "Deepak";
+
+console.log(user1); // Harkirat
+console.log(user2); // Bharat
+console.log(user3); // Deepak
+
+
+// Array
+let users = ["Harkirat", "Bharat", "Deepak"];
+
+console.log(users); // ["Harkirat", "Bharat", "Deepak"]
+
+console.log(users[0]); // Harkirat
+console.log(users[1]); // Bharat
+console.log(users[2]); // Deepak
+console.log(users[3]); // undefined
+
+
+
