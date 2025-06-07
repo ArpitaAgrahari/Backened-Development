1311548e3628b5ae96e6b781ff6e09bd381fe002
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-07 12:45:42 +0530

    fix: add newline at the end of Day2.js for proper file formatting

--- notes/Day2.js
+++ notes/Day2.js
@@ -115,3 +115,15 @@ for (let i = 1; i <= 1000000; i++) {
 
 // print the value of ans
 console.log(ans); // 500000500000
+
+
+
+
+// import an external fs module
+const fs = require('fs');
+
+// read file a.txt and store its content in contents variable
+const contents = fs.readFileSync("a.txt", "utf-8"); // synchronous way of reading file
+
+// print the contents of the file 
+console.log(contents); 
