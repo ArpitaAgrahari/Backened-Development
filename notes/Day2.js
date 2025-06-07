44b68cda93c399593dbedcc8efcc000b14bc7fc7
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-07 12:46:53 +0530

    feat: add asynchronous file reading for a.txt and b.txt

--- notes/Day2.js
+++ notes/Day2.js
@@ -147,3 +147,24 @@ console.log(contents2);
 const contents3 = fs.readFileSync("b.txt", "utf-8");
 // print the contents of the file b.txt
 console.log(contents3);
+
+
+
+
+// import the fs module
+const fs = require("fs");
+
+// read the contents of the file a.txt and store it in the variable contents1 asynchronously using the readFile method
+fs.readFile("a.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
+
+// read the contents of the file b.txt and store it in the variable contents2 asynchronously using the readFile method
+fs.readFile("b.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
+
+// read the contents of the file b.txt and store it in the variable contents3 asynchronously using the readFile method
+fs.readFile("a.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
