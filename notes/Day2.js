8f1488a8895c802976dd124e17426b666d33e90a
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-07 12:48:09 +0530

    feat: add asynchronous file reading for a.txt and b.txt, and update console output

--- notes/Day2.js
+++ notes/Day2.js
@@ -247,3 +247,32 @@ let ans2 = doOperation(1, 2, divide);
 
 // print the result
 console.log(ans2); // 0.5
+
+
+
+
+// import the fs module
+const fs = require("fs");
+
+// read the file a.txt synchronously and print the content
+// let contents = fs.readFileSync("a.txt", "utf8"); // synchronous
+// console.log(contents);
+
+// function to read the file asynchronously
+function print(err, data) {
+    console.log(data);
+}
+
+// read the file a.txt asynchronously and print the content
+// file path, encoding, callback function
+fs.readFile("a.txt", "utf8", print); // asynchronous
+
+// read the file b.txt asynchronously and print the content
+fs.readFile("b.txt", "utf8", print); // asynchronous
+
+console.log("Done!");
+
+
+// fs.readFile("a.txt", "utf-8", function (err, contents) {
+//     console.log(contents);
+// });
