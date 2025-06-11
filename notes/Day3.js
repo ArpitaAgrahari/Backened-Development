aba7af1c6be843f1f45ffba810c6cb6e8b678864
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-11 13:18:43 +0530

    feat: add callback hell example and promisified version in Day3.js

--- notes/Day3.js
+++ notes/Day3.js
@@ -304,3 +304,65 @@ setTimeout(function () {
   }, 3000);
 }, 1000);
 */
+
+// callback hell example - nested callbacks
+// function to print hello there
+function step3Done() {
+  console.log("hello there");
+}
+
+// function to print hello after 3 seconds and call step3Done after 5 seconds
+function step2Done() {
+  console.log("hello");
+
+  // call step3Done after 5 seconds
+  setTimeout(step3Done, 5000);
+}
+
+// function to print hi after 1 second and call step2Done after 3 seconds
+function step1Done() {
+  console.log("hi");
+
+  // call step2Done after 3 seconds
+  setTimeout(step2Done, 3000);
+}
+
+// call step1Done after 1 second
+setTimeout(step1Done, 1000);
+
+/*
+// Promisified Version
+function setTimeoutPromisified(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+setTimeoutPromisified(1000).then(function () {
+  console.log("hi");
+  setTimeoutPromisified(3000).then(function () {
+    console.log("hello");
+    setTimeoutPromisified(5000).then(function () {
+      console.log("hello there");
+    });
+  });
+});
+*/
+
+/*
+// Promisified Version
+function setTimeoutPromisified(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+setTimeoutPromisified(1000)
+  .then(function () {
+    console.log("hi");
+    return setTimeoutPromisified(3000);
+  })
+  .then(function () {
+    console.log("hello");
+    return setTimeoutPromisified(5000);
+  })
+  .then(function () {
+    console.log("hello there");
+  });
+*/
