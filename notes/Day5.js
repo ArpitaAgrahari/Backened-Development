a8c0ac8c8ee0e1610bf2ef3ff03c23ba896b3d67
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-12 13:41:48 +0530

    feat: add HTML structure and JavaScript functionality for Todo list in Day5.js

--- notes/Day5.js
+++ notes/Day5.js
@@ -27,3 +27,66 @@
 
 
 
+
+
+
+
+
+//FEtching data 
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DOM Manipulation- Fetching Elements</title>
+  </head>
+  <body>
+    <h1>Todo List</h1>
+
+    <h4 id="todo">1. Take class</h4>
+    <h4 id="todo">2. Go out to eat</h4>
+    <h4 id="todo">3. Go to gym</h4>
+
+    <div>
+      <input type="text" placeholder="Add your todo here..." />
+      <button id="btn" onclick="addTodo()">Add Todo</button>
+    </div>
+
+    <script src="./index.js"></script>
+  </body>
+</html>
+
+
+// var name = "Bharat";
+// console.log(name);
+
+// console.log(document);
+
+/**
+ * querySelector - returns the first element that matches a specified CSS selector(s) in the document.
+ * Syntax: element = document.querySelector(selectors);
+ * 
+ * 
+ */
+
+
+function addTodo() {
+  const inputEle = document.querySelector("input");
+  const value = inputEle.value;
+  console.log(inputEle);
+  console.log(value);
+}
+
+const h4Ele = document.querySelector("h4");
+console.log(h4Ele);
+
+const h4EleAll = document.querySelectorAll("h4");
+console.log(h4EleAll);
+console.log(h4EleAll[1]);
+
+const btn = document.getElementById("btn");
+console.log(btn);
+
+const todo = document.getElementsByClassName("todo");
+console.log(todo);
