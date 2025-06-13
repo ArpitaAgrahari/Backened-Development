e49e14087e8c72acea53b76ef20d87a5a2dfd60f
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-13 14:31:57 +0530

    feat: add dynamic counter functionality and update header in Day5.js

--- notes/Day5.js
+++ notes/Day5.js
@@ -120,3 +120,31 @@ console.log(todo);
 </html>
 
 
+document.querySelectorAll("h4")[1].innerHTML = "Hello World";
+
+let counter = 0;
+
+function callback() {
+  const element = document.querySelector("h2");
+  element.innerHTML = counter;
+  // console.log(counter);
+  counter++;  
+}
+
+setInterval(callback, 1000);
+
+
+/*
+let counter = 0;
+
+function callback() {
+  const element = document.querySelector("h2");
+  element.innerHTML = counter;
+  
+  setTimeout(callback, 1000);
+  
+  counter++;  
+}
+
+callback();
+*/
