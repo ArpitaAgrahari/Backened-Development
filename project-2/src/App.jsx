94bcd43e0e7ca385be724d0de79f1353c82410a7
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-15 22:46:36 +0530

    fix: comment out unused Greeting function and correct export statement

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -6,14 +6,17 @@
 //   );
 // }
 
+// export default Greeting();
+
+
+//USING ARROW 
+
+// const Greeting=()=>{
+//   return(
+//     <h1>
+//       this is arraow fucntion
+//     </h1>
+//   );
+// }
+
 // export default Greeting;
-
-
-
-const Greeting=()=>{
-  return(
-    <h1>
-      this is arraow fucntion
-    </h1>
-  );
-}
