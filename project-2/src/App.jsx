2adab186cfb671f3194a750b3caa710d062eb88f
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-17 12:27:02 +0530

    refactor: simplify BookList component by removing unused code and consolidating Book details

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -71,6 +71,41 @@
 
 
 
+//ONE WAY
+
+// import './App.css';
+
+// function BookList(){
+//   return(
+//     <section class='booklist'>
+//       <Book/>
+//       <Book/>
+//       <Book/>
+//       <Book/>
+//     </section>
+//   );
+// }
+// const Book = () =>{
+//   return(
+//     <article class="book">
+//       <Image/>
+//       <Title/>
+//       <Author/>
+//     </article>
+//   );
+// }
+// const Image = ()=>{
+//   return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJHrQ4nhsrLaoOY0JMhK27pCcFKebdDfKlwg&s" alt="Book Image" />
+// }
+// const Title= ()=> <h1>A Single Spy</h1>;
+// //oter ways to add css in mostly ui frameworks it is strictly take over the css if written like this over the index.css u have applied 
+// const Author = () => <h1 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>William Christie</h1>
+
+// export default BookList;
+
+
+
+
 
 
 import './App.css';
@@ -79,26 +114,24 @@ function BookList(){
   return(
     <section class='booklist'>
       <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
     </section>
   );
 }
 const Book = () =>{
+
+  const title='A Single Spy'
+  const author='William Christie'
+
   return(
     <article class="book">
-      <Image/>
-      <Title/>
-      <Author/>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJHrQ4nhsrLaoOY0JMhK27pCcFKebdDfKlwg&s" alt="Book Image" />
+      <h1>{title}</h1>
+      <h1 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>{author}</h1>
+      {/* to upper case auhtor name*/}
+      {/* <h1 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>{author.toUpperCase()}</h1> */}
+      <p>{6+6}</p>
     </article>
   );
 }
-const Image = ()=>{
-  return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJHrQ4nhsrLaoOY0JMhK27pCcFKebdDfKlwg&s" alt="Book Image" />
-}
-const Title= ()=> <h1>A Single Spy</h1>;
-//oter ways to add css in mostly ui frameworks it is strictly take over the css if written like this over the index.css u have applied 
-const Author = () => <h1 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>William Christie</h1>
 
 export default BookList;
