73f72e0aa83467f6eaa92f36219c7ca19add2343
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-17 12:18:41 +0530

    fix: correct Author component styling and remove outdated comment

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -98,6 +98,7 @@ const Image = ()=>{
   return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJHrQ4nhsrLaoOY0JMhK27pCcFKebdDfKlwg&s" alt="Book Image" />
 }
 const Title= ()=> <h1>A Single Spy</h1>;
-const Author = () => <h1>William Christie</h1>
+//oter ways to add css in mostly ui frameworks it is strictly take over the css if written like this over the index.css u have applied 
+const Author = () => <h1 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>William Christie</h1>
 
 export default BookList;
