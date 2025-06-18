2d7e18f1ffbf128bca90abfcf543c1486544e34d
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-18 23:55:26 +0530

    Add nodemon for development and express as a dependency; create .gitignore for node_modules

--- crudApp/index.js
+++ crudApp/index.js
@@ -1,1 +1,15 @@
-console.log('hello there');
+const express= require('express');
+const app=express()
+
+
+const PORT=3000
+
+app.listen(PORT, ()=>{
+    console.log(`server is listening to port ${PORT}`);
+})
+
+
+
+app.get('/',(req,res)=>{
+    res.send('hello from node API');
+});
